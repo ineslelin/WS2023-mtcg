dc158bf0d047d319741b59baf545234d7f7bd559
Ines Lintner <ines.l.lintner@gmail.com>
2023-12-08 19:12:35 +0100

    created a database

--- ws2023-mtcg/Server/Repository/IRepository.cs
+++ ws2023-mtcg/Server/Repository/IRepository.cs
@@ -6,14 +6,12 @@ using System.Threading.Tasks;
 
 namespace ws2023_mtcg.Server.Repository
 {
+    // db connectionstring should be "Host=localhost;Database=mtcgdb;Username=admin;Password=1234"
     internal interface IRepository<T>
     {
-        T Get(int id);
-
-        void Add(T entity);
-
-        void Update(T entity, string[] parameters);
-
-        void Delete(int id);
+        T Read(int id);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T t);
     }
 }
