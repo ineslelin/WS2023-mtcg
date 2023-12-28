d219cf4be63c1c519356413f5b764d215c6a455d
Ines Lintner <ines.l.lintner@gmail.com>
2023-12-28 18:11:07 +0100

    created a little handler to use with the curl script that creates and drops the tables

--- ws2023-mtcg/Server/DbCommands.cs
+++ ws2023-mtcg/Server/DbCommands.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ws2023_mtcg.FightLogic.Enums;
+using ws2023_mtcg.Models;
 
 namespace ws2023_mtcg.Server
 {
