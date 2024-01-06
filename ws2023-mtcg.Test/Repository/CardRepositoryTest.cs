3104660a831f0e3aaeaaa1f839df472a401d3c62
Ines Lintner <ines.l.lintner@gmail.com>
2024-01-06 21:34:00 +0100

    added unit tests for each element and type card, a bit redundant imo but a test is a test

--- ws2023-mtcg.Test/Repository/CardRepositoryTest.cs
+++ ws2023-mtcg.Test/Repository/CardRepositoryTest.cs
@@ -67,5 +67,241 @@ namespace ws2023_mtcg.Test.Repository
             cardRepositoryMock.Verify(repo => repo.Create(It.IsAny<Cards>()), Times.Once);
             cardRepositoryMock.Verify(repo => repo.Create(newCard), Times.Once);
         }
+
+        [Test]
+        public void CheckIfCardFireType()
+        {
+            // arrange
+            var cardRepositoryMock = new Mock<ICardRepository<Cards>>();
+
+            cardRepositoryMock.Setup(repo => repo.Read("TestId"))
+                .Returns(new Cards
+                {
+                    Id = "TestId",
+                    Element = ElementType.fire
+                });
+
+            // act
+            var actual = cardRepositoryMock.Object.Read("TestId");
+
+            // assert
+            Assert.That(actual, Is.Not.Null);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual.Id, Is.EqualTo("TestId"));
+                Assert.That(actual.Element, Is.EqualTo(ElementType.fire));
+            });
+        }
+
+        [Test]
+        public void CheckIfCardWaterType()
+        {
+            // arrange
+            var cardRepositoryMock = new Mock<ICardRepository<Cards>>();
+
+            cardRepositoryMock.Setup(repo => repo.Read("TestId"))
+                .Returns(new Cards
+                {
+                    Id = "TestId",
+                    Element = ElementType.water
+                });
+
+            // act
+            var actual = cardRepositoryMock.Object.Read("TestId");
+
+            // assert
+            Assert.That(actual, Is.Not.Null);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual.Id, Is.EqualTo("TestId"));
+                Assert.That(actual.Element, Is.EqualTo(ElementType.water));
+            });
+        }
+
+        [Test]
+        public void CheckIfCardRegularType()
+        {
+            // arrange
+            var cardRepositoryMock = new Mock<ICardRepository<Cards>>();
+
+            cardRepositoryMock.Setup(repo => repo.Read("TestId"))
+                .Returns(new Cards
+                {
+                    Id = "TestId",
+                    Element = ElementType.normal
+                });
+
+            // act
+            var actual = cardRepositoryMock.Object.Read("TestId");
+
+            // assert
+            Assert.That(actual, Is.Not.Null);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual.Id, Is.EqualTo("TestId"));
+                Assert.That(actual.Element, Is.EqualTo(ElementType.normal));
+            });
+        }
+
+        [Test]
+        public void CheckIfCardElectricType()
+        {
+            // arrange
+            var cardRepositoryMock = new Mock<ICardRepository<Cards>>();
+
+            cardRepositoryMock.Setup(repo => repo.Read("TestId"))
+                .Returns(new Cards
+                {
+                    Id = "TestId",
+                    Element = ElementType.electric
+                });
+
+            // act
+            var actual = cardRepositoryMock.Object.Read("TestId");
+
+            // assert
+            Assert.That(actual, Is.Not.Null);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual.Id, Is.EqualTo("TestId"));
+                Assert.That(actual.Element, Is.EqualTo(ElementType.electric));
+            });
+        }
+
+        [Test]
+        public void CheckIfCardIceType()
+        {
+            // arrange
+            var cardRepositoryMock = new Mock<ICardRepository<Cards>>();
+
+            cardRepositoryMock.Setup(repo => repo.Read("TestId"))
+                .Returns(new Cards
+                {
+                    Id = "TestId",
+                    Element = ElementType.ice
+                });
+
+            // act
+            var actual = cardRepositoryMock.Object.Read("TestId");
+
+            // assert
+            Assert.That(actual, Is.Not.Null);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual.Id, Is.EqualTo("TestId"));
+                Assert.That(actual.Element, Is.EqualTo(ElementType.ice));
+            });
+        }
+
+        [Test]
+        public void CheckIfCardGrassType()
+        {
+            // arrange
+            var cardRepositoryMock = new Mock<ICardRepository<Cards>>();
+
+            cardRepositoryMock.Setup(repo => repo.Read("TestId"))
+                .Returns(new Cards
+                {
+                    Id = "TestId",
+                    Element = ElementType.grass
+                });
+
+            // act
+            var actual = cardRepositoryMock.Object.Read("TestId");
+
+            // assert
+            Assert.That(actual, Is.Not.Null);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual.Id, Is.EqualTo("TestId"));
+                Assert.That(actual.Element, Is.EqualTo(ElementType.grass));
+            });
+        }
+
+        [Test]
+        public void CheckIfCardMonsterCard()
+        {
+            // arrange
+            var cardRepositoryMock = new Mock<ICardRepository<Cards>>();
+
+            cardRepositoryMock.Setup(repo => repo.Read("TestId"))
+                .Returns(new Cards
+                {
+                    Id = "TestId",
+                    Type = CardType.monster
+                });
+
+            // act
+            var actual = cardRepositoryMock.Object.Read("TestId");
+
+            // assert
+            Assert.That(actual, Is.Not.Null);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual.Id, Is.EqualTo("TestId"));
+                Assert.That(actual.Type, Is.EqualTo(CardType.monster));
+            });
+        }
+
+        [Test]
+        public void CheckIfCardSpellCard()
+        {
+            // arrange
+            var cardRepositoryMock = new Mock<ICardRepository<Cards>>();
+
+            cardRepositoryMock.Setup(repo => repo.Read("TestId"))
+                .Returns(new Cards
+                {
+                    Id = "TestId",
+                    Type = CardType.spell
+                });
+
+            // act
+            var actual = cardRepositoryMock.Object.Read("TestId");
+
+            // assert
+            Assert.That(actual, Is.Not.Null);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual.Id, Is.EqualTo("TestId"));
+                Assert.That(actual.Type, Is.EqualTo(CardType.spell));
+            });
+        }
+
+        [Test]
+        public void CheckIfCardCurseCard()
+        {
+            // arrange
+            var cardRepositoryMock = new Mock<ICardRepository<Cards>>();
+
+            cardRepositoryMock.Setup(repo => repo.Read("TestId"))
+                .Returns(new Cards
+                {
+                    Id = "TestId",
+                    Element = ElementType.curse,
+                    Type = CardType.curse
+                });
+
+            // act
+            var actual = cardRepositoryMock.Object.Read("TestId");
+
+            // assert
+            Assert.That(actual, Is.Not.Null);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(actual.Id, Is.EqualTo("TestId"));
+                Assert.That(actual.Element, Is.EqualTo(ElementType.curse));
+                Assert.That(actual.Type, Is.EqualTo(CardType.curse));
+            });
+        }
     }
 }
